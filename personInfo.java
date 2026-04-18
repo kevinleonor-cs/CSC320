3404686e85e2953352cb262630fa723c6b5fc03f
kevinleonor-cs <kevin.leonor@csuglobal.edu>
2026-04-17 17:53:57 -0700

    Add files via upload

---
+++ personInfo.java
@@ -0,0 +1,15 @@
+public class personInfo {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Smith";
+        String streetAddress = "123 Main Street";
+        String city = "Springfield";
+        String zipCode = "12345";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
---
+++ psuedocode.vb
@@ -0,0 +1,13 @@
+PROGRAM PrintPersonInfo
+    CALLIT firstName AS String = "John"
+    CALLIT lastName AS String = "Smith"
+    CALLIT streetAddress AS String = "123 Main Street"
+    CALLIT city AS String = "Springfield"
+    CALLIT zipCode AS String = "12345"
+
+    SHOWIT firstName
+    SHOWIT lastName
+    SHOWIT streetAddress
+    SHOWIT city
+    SHOWIT zipCode
+END PROGRAM
